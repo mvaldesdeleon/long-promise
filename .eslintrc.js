b8ebe64dd0502a91e15729981723065ac17e917f
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-06-01 21:52:27 +0200

    Adds `.eslintrc.js`.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
